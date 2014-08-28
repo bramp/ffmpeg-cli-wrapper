de0c0bcb2e6e9673e17703e7838de75c2d949cd7
Matt Faulkner <matthew.faulkner@genesys.com>
2014-08-28 12:56:30 +0100

    A test case which shows mapping failures

---
+++ src/test/java/net/bramp/ffmpeg/modelmapper/MapperTest.java
@@ -0,0 +1,29 @@
+package net.bramp.ffmpeg.modelmapper;
+
+import net.bramp.ffmpeg.options.AudioEncodingOptions;
+import net.bramp.ffmpeg.options.EncodingOptions;
+import net.bramp.ffmpeg.options.MainEncodingOptions;
+import net.bramp.ffmpeg.options.VideoEncodingOptions;
+import net.bramp.ffmpeg.builder.FFmpegOutputBuilder;
+import net.bramp.ffmpeg.modelmapper.Mapper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapperTest {
+
+	@Test
+	public void testMapping() {
+		MainEncodingOptions main = new MainEncodingOptions("mp4", 0L, null);
+		AudioEncodingOptions audio = new AudioEncodingOptions(false, null, 0, 0, null, 0, 0);
+		VideoEncodingOptions video = new VideoEncodingOptions(true, null, null, 320, 0, 0, null, "scale='320:trunc(ow/a/2)*2'", "");
+		
+		EncodingOptions options = new EncodingOptions(main, audio, video);
+		
+		FFmpegOutputBuilder mappedObj = new FFmpegOutputBuilder();
+		
+		Mapper.map(options, mappedObj);
+		
+		Assert.assertNotNull(mappedObj);
+	}
+}
