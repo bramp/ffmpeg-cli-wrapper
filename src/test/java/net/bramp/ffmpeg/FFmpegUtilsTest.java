81383c9fbf2b25533d370d1c9cbd1fcebd77da74
Andrew Brampton <bramp@google.com>
2016-02-27 13:39:00 -0800

    Added unit test for FFmpegUtils.millisecondsToString.

---
+++ src/test/java/net/bramp/ffmpeg/FFmpegUtilsTest.java
@@ -0,0 +1,16 @@
+package net.bramp.ffmpeg;
+
+import org.junit.Test;
+
+import static net.bramp.ffmpeg.FFmpegUtils.millisecondsToString;
+import static org.junit.Assert.*;
+
+public class FFmpegUtilsTest {
+
+  @Test
+  public void testMillisecondsToString() throws Exception {
+    assertEquals("00:01:03.123", millisecondsToString(63123));
+    assertEquals("00:01:03", millisecondsToString(63000));
+    assertEquals("01:23:45.678", millisecondsToString(5025678));
+  }
+}
