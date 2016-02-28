0e573ec18a7ce2bd1661de6aa7ddc80e63c552b9
Andrew Brampton <bramp@google.com>
2016-02-28 12:48:24 -0800

    Removed all the non-apidoc files.

--- src/main/java/net/bramp/ffmpeg/probe/FFmpegError.java
+++
@@ -1,6 +0,0 @@
-package net.bramp.ffmpeg.probe;
-
-public class FFmpegError {
-  public int code;
-  public String string;
-}
--- src/test/resources/net/bramp/ffmpeg/samples/README.md
+++
@@ -1,3 +0,0 @@
-Sample media:
-
-big_buck_bunny_720p_1mb.mp4 - Big Buck Bunny - Creative Commons Attribution 3.0
