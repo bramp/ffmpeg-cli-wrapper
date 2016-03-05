5a805891a1aff32fd3f2a19de3498241bd4a78e3
Andrew Brampton <bramp@google.com>
2016-03-05 13:06:23 -0800

    Updated the README.md to be correct closing #7. Additionally added a unit test with the README code to ensure we catch this next time we break the API.

--- README.md
+++ README.md
@@ -26,27 +26,34 @@ FFmpeg ffmpeg = new FFmpeg("/path/to/ffmpeg");
 FFprobe ffprobe = new FFprobe("/path/to/ffprobe");
 
 FFmpegBuilder builder = new FFmpegBuilder()
-    .setInput(in)
-    .overrideOutputFiles(true)
-    .addOutput("output.mp4")
-        .setFormat("mp4")
-        .setTargetSize(250000)
-        
-        .disableSubtitle()
-        
-        .setAudioChannels(1)
-        .setAudioCodec("libfdk_aac")
-        .setAudioRate(48000)
-        .setAudioBitrate(32768)
-        
-        .setVideoCodec("libx264")
-        .setVideoFramerate(Fraction.getFraction(24, 1))
-        .setVideoResolution(640, 480)
-        
-        .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
-        .done();
+
+  .setInput("input.mp4")     // Filename, or a FFmpegProbeResult
+  .overrideOutputFiles(true) // Override the output if it exists
+
+  .addOutput("output.mp4")   // Filename for the destination
+    .setFormat("mp4")        // Format is inferred from filename, or can be set
+    .setTargetSize(250_000)  // Aim for a 250KB file
+
+    .disableSubtitle()       // No subtiles
+
+    .setAudioChannels(1)         // Mono audio
+    .setAudioCodec("aac")        // using the aac codec
+    .setAudioSampleRate(48_000)  // at 48KHz
+    .setAudioBitRate(32768)      // at 32 kbit/s
+
+    .setVideoCodec("libx264")     // Video using x264
+    .setVideoFrameRate(24, 1)     // at 24 frames per second
+    .setVideoResolution(640, 480) // at 640x480 resolution
+
+    .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+    .done();
 
 FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+// Run a one-pass encode
+executor.createJob(builder).run();
+
+// Or run a two-pass encode (which is slower at the cost of better quality)
 executor.createTwoPassJob(builder).run();
 ```
 
---
+++ src/test/java/net/bramp/ffmpeg/ReadmeTest.java
@@ -0,0 +1,56 @@
+package net.bramp.ffmpeg;
+
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import org.apache.commons.lang3.math.Fraction;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Ensures the example in the readme continues to work
+ */
+public class ReadmeTest {
+
+  @Test
+  public void test() throws IOException {
+    FFmpeg ffmpeg = new FFmpeg(FFmpeg.DEFAULT_PATH);
+    FFprobe ffprobe = new FFprobe(FFprobe.DEFAULT_PATH);
+
+    // String in = Samples.big_buck_bunny_720p_1mb;
+    FFmpegProbeResult in = ffprobe.probe(Samples.big_buck_bunny_720p_1mb);
+
+    // @formatter:off
+    FFmpegBuilder builder = new FFmpegBuilder()
+
+      .setInput(in)              // Filename, or a FFmpegProbeResult
+      .overrideOutputFiles(true) // Override the output if it exists
+
+      .addOutput("output.mp4")   // Filename for the destination
+        .setFormat("mp4")        // Format is inferred from filename, or can be set
+        .setTargetSize(250_000)  // Aim for a 250KB file
+
+        .disableSubtitle()       // No subtiles
+
+        .setAudioChannels(1)         // Mono audio
+        .setAudioCodec("aac")        // using the aac codec
+        .setAudioSampleRate(48_000)  // at 48KHz
+        .setAudioBitRate(32768)      // at 32 kbit/s
+
+        .setVideoCodec("libx264")     // Video using x264
+        .setVideoFrameRate(24, 1)     // at 24 frames per second
+        .setVideoResolution(640, 480) // at 640x480 resolution
+
+        .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+        .done();
+    // @formatter:on
+
+    FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+    // Run a one-pass encode
+    executor.createJob(builder).run();
+
+    // Or run a two-pass encode (which is slower at the cost of better quality
+    executor.createTwoPassJob(builder).run();
+  }
+}
