25b00e4a03036a1f33aa739cafc81a79e50b7169
Andrew Brampton <bramp@google.com>
2016-03-06 09:16:30 -0800

    Added new sample provided by a user.

--- src/test/java/net/bramp/ffmpeg/fixtures/Samples.java
+++ src/test/java/net/bramp/ffmpeg/fixtures/Samples.java
@@ -6,5 +6,11 @@ public abstract class Samples {
   public static final String TEST_PREFIX = "src/test/resources/net/bramp/ffmpeg/samples/";
   public static final String big_buck_bunny_720p_1mb = TEST_PREFIX + "big_buck_bunny_720p_1mb.mp4";
 
+  // We don't have the following files
+  public static final String FAKE_PREFIX = "fake/";
+
+  public static final String always_on_my_mind = FAKE_PREFIX + "Always On My Mind [Program Only] - Adelén.mp4";
+
+
   public static final String output_mp4 = "output.mp4"; // TODO Change to a temp directory
 }
