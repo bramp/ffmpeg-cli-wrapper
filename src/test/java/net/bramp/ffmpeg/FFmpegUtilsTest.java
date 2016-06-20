6051444c1b0e58f027104f7f79b2e8c3ba3022ab
Andrew Brampton <bramp@google.com>
2016-06-19 21:39:06 -0700

    Added new parseBitrate method.

--- src/test/java/net/bramp/ffmpeg/FFmpegUtilsTest.java
+++ src/test/java/net/bramp/ffmpeg/FFmpegUtilsTest.java
@@ -3,6 +3,7 @@ package net.bramp.ffmpeg;
 import org.junit.Test;
 
 import static net.bramp.ffmpeg.FFmpegUtils.millisecondsToString;
+import static net.bramp.ffmpeg.FFmpegUtils.parseBitrate;
 import static org.junit.Assert.assertEquals;
 
 public class FFmpegUtilsTest {
@@ -13,5 +14,12 @@ public class FFmpegUtilsTest {
     assertEquals("00:01:03", millisecondsToString(63000));
     assertEquals("01:23:45.678", millisecondsToString(5025678));
     assertEquals("00:00:00", millisecondsToString(0));
+  }
+
+  @Test
+  public void testParseBitrate() throws Exception {
+    assertEquals(12300, parseBitrate("12.3kbits/s"));
+    assertEquals(1000, parseBitrate("1kbits/s"));
+    assertEquals(123, parseBitrate("0.123kbits/s"));
   }
 }
