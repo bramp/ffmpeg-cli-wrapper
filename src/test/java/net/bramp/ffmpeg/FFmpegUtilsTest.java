4006ae63904279a3f72873ad23fcd5d9a7fc94cb
stonio <stonio@users.noreply.github.com>
2016-06-23 16:35:14 +0200

    More tests on millisecondsToString(long) (#40)

--- src/test/java/net/bramp/ffmpeg/FFmpegUtilsTest.java
+++ src/test/java/net/bramp/ffmpeg/FFmpegUtilsTest.java
@@ -14,6 +14,17 @@ public class FFmpegUtilsTest {
     assertEquals("00:01:03", millisecondsToString(63000));
     assertEquals("01:23:45.678", millisecondsToString(5025678));
     assertEquals("00:00:00", millisecondsToString(0));
+    assertEquals("2562047788015:12:55.807", millisecondsToString(Long.MAX_VALUE));
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testMillisecondsToStringNegative() throws Exception {
+      millisecondsToString(-1);
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testMillisecondsToStringNegativeMinValue() throws Exception {
+      millisecondsToString(Long.MIN_VALUE);
   }
 
   @Test
