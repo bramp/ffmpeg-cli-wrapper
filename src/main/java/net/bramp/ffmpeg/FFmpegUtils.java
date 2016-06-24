559c42cb091880ed9bbb78306e01a37df5b8bd89
stonio <stonio@users.noreply.github.com>
2016-06-24 18:21:36 +0200

    FFmpegUtils - Restore class as final

--- src/main/java/net/bramp/ffmpeg/FFmpegUtils.java
+++ src/main/java/net/bramp/ffmpeg/FFmpegUtils.java
@@ -20,7 +20,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 /**
  * Helper class with commonly used methods
  */
-public abstract class FFmpegUtils {
+public final class FFmpegUtils {
 
   final static Gson gson = FFmpegUtils.setupGson();
 
