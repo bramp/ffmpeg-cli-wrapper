bramp/ffmpeg-cli-wrapper

commit 4b4de30e754a0bf525333cb49ab98f0f69ba0b0e

Add custom passlog directory when passlogfile is setted (#67)

src/main/java/net/bramp/ffmpeg/builder/FFmpegBuilder.java
@@ -56,6 +56,7 @@ public class FFmpegBuilder {
   // Global Settings
   boolean override = true;
   int pass = 0;
+  String pass_directory;
   String pass_prefix;
   Verbosity verbosity = Verbosity.ERROR;
   URI progress;
@@ -84,6 +85,11 @@ public class FFmpegBuilder {
 
   public FFmpegBuilder setPass(int pass) {
     this.pass = pass;
+    return this;
+  }
+
+  public FFmpegBuilder setPassDirectory(String directory) {
+    this.pass_directory = checkNotNull(directory);
     return this;
   }
 
@@ -172,7 +178,7 @@ public class FFmpegBuilder {
 
   /**
    * Adds new output file.
-   * 
+   *
    * @param filename output file path
    * @return A new {@link FFmpegOutputBuilder}
    */
@@ -240,7 +246,7 @@ public class FFmpegBuilder {
       args.add("-pass", Integer.toString(pass));
 
       if (pass_prefix != null) {
-        args.add("-passlogfile", pass_prefix);
+        args.add("-passlogfile", pass_directory + pass_prefix);
       }
     }
 

commit bc620347025ef18ad9092ad8cea0fec37f77f98d

Fixes a bug introduced by #67 where the string "null" would be used in the pass prefix, if pass_directory was unset. This would have been caught with the most simple of unit tests!

src/main/java/net/bramp/ffmpeg/builder/FFmpegBuilder.java
@@ -56,7 +56,7 @@ public class FFmpegBuilder {
   // Global Settings
   boolean override = true;
   int pass = 0;
-  String pass_directory;
+  String pass_directory = "";
   String pass_prefix;
   Verbosity verbosity = Verbosity.ERROR;
   URI progress;