f26c3c36bfaeba919dd7c0724cd9e04611d3c4da
Andrew Brampton <bramp@google.com>
2016-10-22 23:15:07 -0700

    Minor whitespace change.

--- src/test/java/net/bramp/ffmpeg/builder/FFmpegOutputBuilderCheckValidStreamTest.java
+++ src/test/java/net/bramp/ffmpeg/builder/FFmpegOutputBuilderCheckValidStreamTest.java
@@ -12,22 +12,23 @@ import java.util.List;
 @RunWith(Parameterized.class)
 public class FFmpegOutputBuilderCheckValidStreamTest {
 
+  // @Parameters
   @Parameters(name = "{0}")
   public static List<String> data() {
     return Arrays.asList(
-        // @formatter:off
-        "udp://10.1.0.102:1234",
-        "tcp://127.0.0.1:2000",
-        "udp://236.0.0.1:2000",
-        "rtmp://live.twitch.tv/app/live_",
-        "rtmp:///live/myStream.sdp",
-        "rtp://127.0.0.1:1234",
-        "rtsp://localhost:8888/live.sdp",
-        "rtsp://localhost:8888/live.sdp?tcp",
+      // @formatter:off
+      "udp://10.1.0.102:1234",
+      "tcp://127.0.0.1:2000",
+      "udp://236.0.0.1:2000",
+      "rtmp://live.twitch.tv/app/live_",
+      "rtmp:///live/myStream.sdp",
+      "rtp://127.0.0.1:1234",
+      "rtsp://localhost:8888/live.sdp",
+      "rtsp://localhost:8888/live.sdp?tcp",
 
-        // Some others
-        "UDP://10.1.0.102:1234"
-        // @formatter:on
+      // Some others
+      "UDP://10.1.0.102:1234"
+      // @formatter:on
         );
   }
 
