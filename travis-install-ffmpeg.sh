67579bfa43c9325a9ee9e60b92989a0e061fa212
stonio <stonio@users.noreply.github.com>
2016-12-09 18:01:48 +0100

    Update init script to use HTTPS (#82)
    * Download ffmpeg library via HTTPS
    * Update DEST variable expression

--- travis-install-ffmpeg.sh
+++ travis-install-ffmpeg.sh
@@ -2,11 +2,11 @@
 # Small script to fetch a static ffmpeg
 set -ex
 
-URL=http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
+URL=https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 FILE=$(basename ${URL})
 DIST=$HOME/.dist
 DISTFILE=${DIST}/${FILE}
-DEST=ffmpeg-release-64bit-static
+DEST=${FILE%%.*}
 
 [ -d ${DIST} ] || mkdir ${DIST}
 [ -d ${DEST} ] || mkdir ${DEST}
