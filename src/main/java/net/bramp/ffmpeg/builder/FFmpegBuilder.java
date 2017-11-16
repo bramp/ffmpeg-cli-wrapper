0cec3457ee3279c02ec05015e66903b7ba6984f5
Rick Chen <rick@xempireinc.com>
2017-11-16 15:42:27 -0800

    Fix the audio and video filter arguments (they were flipped)

--- src/main/java/net/bramp/ffmpeg/builder/FFmpegBuilder.java
+++ src/main/java/net/bramp/ffmpeg/builder/FFmpegBuilder.java
@@ -320,11 +320,11 @@ public class FFmpegBuilder {
     }
 
     if (!Strings.isNullOrEmpty(videoFilter)) {
-      args.add("-af", videoFilter);
+      args.add("-vf", videoFilter);
     }
 
     if (!Strings.isNullOrEmpty(audioFilter)) {
-      args.add("-vf", audioFilter);
+      args.add("-af", audioFilter);
     }
 
     for (FFmpegOutputBuilder output : this.outputs) {
