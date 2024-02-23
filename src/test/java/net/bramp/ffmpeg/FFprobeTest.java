e4d5b6e6f1b532b148585df7c6a2ccdb407c6a23
Joel Widmer <joel@widmer.me>
2024-02-23 14:45:31 +0100

    fix: Tests failing on some systems due to filename

--- src/test/java/net/bramp/ffmpeg/FFprobeTest.java
+++ src/test/java/net/bramp/ffmpeg/FFprobeTest.java
@@ -39,7 +39,7 @@ public class FFprobeTest {
         .thenAnswer(new NewProcessAnswer("ffprobe-big_buck_bunny_720p_1mb.mp4"));
 
     when(runFunc.run(argThatHasItem(Samples.always_on_my_mind)))
-        .thenAnswer(new NewProcessAnswer("ffprobe-Always On My Mind [Program Only] - Adelén.mp4"));
+        .thenAnswer(new NewProcessAnswer("ffprobe-Always On My Mind [Program Only] - Adelen.mp4"));
 
     when(runFunc.run(argThatHasItem(Samples.start_pts_test)))
         .thenAnswer(new NewProcessAnswer("ffprobe-start_pts_test"));
@@ -122,7 +122,7 @@ public class FFprobeTest {
     // Test a UTF-8 name
     assertThat(
         info.getFormat().filename,
-        is("c:\\Users\\Bob\\Always On My Mind [Program Only] - Adelén.mp4"));
+        is("c:\\Users\\Bob\\Always On My Mind [Program Only] - Adelen.mp4"));
 
     // System.out.println(FFmpegUtils.getGson().toJson(info));
   }
