3c22e839b78b04d7d844756130edcd476bb06ae3
van1164 <vkn123@naver.com>
2024-03-20 17:58:32 +0900

    feat: add addFormatArgs() for hls argument

--- src/main/java/net/bramp/ffmpeg/builder/FFmpegHlsOutputBuilder.java
+++ src/main/java/net/bramp/ffmpeg/builder/FFmpegHlsOutputBuilder.java
@@ -1,29 +1,38 @@
 package net.bramp.ffmpeg.builder;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import javax.annotation.CheckReturnValue;
+import static com.google.common.base.Preconditions.*;
+import static net.bramp.ffmpeg.FFmpegUtils.toTimecode;
 import static net.bramp.ffmpeg.Preconditions.checkNotEmpty;
 import java.util.concurrent.TimeUnit;
 
-public class FFmpegHlsOutputBuilder extends FFmpegOutputBuilder{
+public class FFmpegHlsOutputBuilder extends FFmpegOutputBuilder {
+
+    public Long hls_time;
+    public String hls_segment_filename;
+    public Long hls_init_time;
+    public Integer hls_list_size;
+    public String hls_base_url;
+
 
     protected FFmpegHlsOutputBuilder(FFmpegBuilder parent, String filename) {
         super(parent, filename);
     }
 
 
-
     /**
-     Set the target segment length. Default value is 2.
+     * Set the target segment length. Default value is 2.
      *
      * @param duration hls_time to set
      * @return {@link FFmpegHlsOutputBuilder}
      */
-    public FFmpegHlsOutputBuilder setHlsTime(Long duration, TimeUnit units){
+    public FFmpegHlsOutputBuilder setHlsTime(long duration, TimeUnit units) {
         checkNotNull(units);
-        extra_args.add("-hls_time");
-        extra_args.add(units.toMillis(duration));
-        return this;
+        this.hls_time = units.toMillis(duration);
+
+        return getThis();
     }
 
     /**
@@ -34,68 +43,81 @@ public class FFmpegHlsOutputBuilder extends FFmpegOutputBuilder{
      * @param filename hls_segment_file_name to set
      * @return {@link FFmpegHlsOutputBuilder}
      */
-    public FFmpegHlsOutputBuilder setHlsSegmentFileName(String filename){
-        filename = checkNotEmpty(filename, "filename must not be empty");
-        extra_args.add("-hls_segment_filename");
-        extra_args.add(filename);
-        return this;
+    public FFmpegHlsOutputBuilder setHlsSegmentFileName(String filename) {
+        this.hls_segment_filename = checkNotEmpty(filename, "filename must not be empty");
+
+        return getThis();
     }
 
     /**
      * <strong>Segment will be cut on the next key frame after this time has passed on the first m3u8 list.</strong> <br>
-     * Duration Examples <br>
-     * Default :  "0" <br>
-     * The following examples are all valid time duration:
-     * <br>
-     * "55" -> 55 seconds
-     * <br>
-     * "0.2" -> 0.2 seconds
-     * <br>
-     * "200ms" -> 200 milliseconds, that’s 0.2s
-     * <br>
-     * "200000us" -> 200000 microseconds, that’s 0.2s
-     * <br>
-     * "12:03:45" -> 12 hours, 03 minutes and 45 seconds
-     * <br>
-     * "23.189" -> 23.189 seconds
      *
      * @param duration hls_init_time to set
      * @return {@link FFmpegHlsOutputBuilder}
      */
-    public FFmpegHlsOutputBuilder setHlsInitTime(Long duration, TimeUnit units){
+    public FFmpegHlsOutputBuilder setHlsInitTime(long duration, TimeUnit units) {
         checkNotNull(units);
-        extra_args.add("-hls_init_time");
-        extra_args.add(duration);
-        return this;
+        this.hls_init_time = units.toMillis(duration);
+
+        return getThis();
     }
 
     /**
      * <strong>Set the maximum number of playlist entries. If set to 0 the list file will contain all the segments .</strong> <br>
      * Default value is 5 <br>
+     *
      * @param size hls_time to set
      * @return {@link FFmpegHlsOutputBuilder}
      */
-    public FFmpegHlsOutputBuilder setHlsListSize(int size){
+    public FFmpegHlsOutputBuilder setHlsListSize(int size) {
         checkArgument(size >= 0, "Size cannot be less than 0.");
-        extra_args.add("-hls_list_size");
-        extra_args.add(Integer.toString(size));
-        return this;
+        this.hls_list_size = size;
+
+        return getThis();
     }
 
     /**
      * <strong>Append baseurl to every entry in the playlist.
      * Useful to generate playlists with absolute paths. <br>
      * Note that the playlist sequence number must be unique for each segment and it is not to be confused with the segment filename sequence number which can be cyclic, for example if the wrap option is specified.</strong> <br>
+     *
      * @param baseurl hls_base_url to set
      * @return {@link FFmpegHlsOutputBuilder}
      */
-    public FFmpegHlsOutputBuilder setHlsBaseUrl(String baseurl){
-        baseurl = checkNotEmpty(baseurl, "baseurl must not be empty");
-        extra_args.add("-hls_base_url");
-        extra_args.add(baseurl);
-        return this;
+    public FFmpegHlsOutputBuilder setHlsBaseUrl(String baseurl) {
+        this.hls_base_url = checkNotEmpty(baseurl, "baseurl must not be empty");
+
+        return getThis();
     }
 
+    @Override
+    protected void addFormatArgs(ImmutableList.Builder<String> args) {
+        super.addFormatArgs(args);
+        if (hls_time != null) {
+            args.add("-hls_time", toTimecode(hls_time, TimeUnit.MILLISECONDS));
+        }
 
+        if (!Strings.isNullOrEmpty(hls_segment_filename)) {
+            args.add("-hls_segment_filename", hls_segment_filename);
+        }
 
+        if (hls_init_time != null) {
+            args.add("-hls_init_time",toTimecode(hls_init_time, TimeUnit.MILLISECONDS));
+        }
+
+        if (hls_list_size != null) {
+            args.add("-hls_list_size",hls_list_size.toString());
+        }
+
+        if (!Strings.isNullOrEmpty(hls_base_url)){
+            args.add("-hls_base_url",hls_base_url);
+        }
+    }
+
+    @CheckReturnValue
+    @Override
+    protected FFmpegHlsOutputBuilder getThis() {
+        return this;
+    }
 }
+
