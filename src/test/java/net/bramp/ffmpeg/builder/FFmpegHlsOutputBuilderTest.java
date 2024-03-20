e900e1d52cafb813a165ee06f30636417b2621d5
van1164 <vkn123@naver.com>
2024-03-20 17:55:55 +0900

    test: fix test code to match the code

--- src/test/java/net/bramp/ffmpeg/builder/FFmpegHlsOutputBuilderTest.java
+++ src/test/java/net/bramp/ffmpeg/builder/FFmpegHlsOutputBuilderTest.java
@@ -1,7 +1,9 @@
 package net.bramp.ffmpeg.builder;
 
 import com.google.common.collect.ImmutableList;
-import junit.framework.TestCase;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 import java.io.IOException;
@@ -19,16 +21,17 @@ public class FFmpegHlsOutputBuilderTest {
                 new FFmpegBuilder()
                         .setInput("input")
                         .addHlsOutput("output.m3u8")
-                        .setHlsTime(5,TimeUnits.MILLISECONDS)
+                        .setHlsTime(5, TimeUnit.MILLISECONDS)
                         .setHlsBaseUrl("test1234/")
                         .setHlsListSize(3)
-                        .setHlsInitTime(3,TimeUnits.MILLISECONDS)
+                        .setHlsInitTime(3, TimeUnit.MILLISECONDS)
                         .setHlsSegmentFileName("file%03d.ts")
                         .done()
                         .build();
         assertEquals(
-                args, ImmutableList.of("-y", "-v", "error", "-i", "input", "-hls_time", "5", "-hls_base_url",
-                        "test1234/", "-hls_list_size", "3", "-hls_init_time", "3", "-hls_segment_filename", "file%03d.ts", "output.m3u8"));
+                args, ImmutableList.of("-y", "-v", "error", "-f", "hls", "-i", "input", "-hls_time", "00:00:00.005",
+                        "-hls_segment_filename", "file%03d.ts", "-hls_init_time", "00:00:00.003",
+                        "-hls_list_size", "3", "-hls_base_url", "test1234/", "output.m3u8"));
     }
 
 
