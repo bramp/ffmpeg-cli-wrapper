d62145c1a1341352d1777770319ba1d3df215de5
Andrew Brampton <github@bramp.net>
2024-03-20 10:32:26 -0700

    Fixed private field access.

--- src/test/java/net/bramp/ffmpeg/info/CodecTest.java
+++ src/test/java/net/bramp/ffmpeg/info/CodecTest.java
@@ -11,43 +11,43 @@ public class CodecTest {
     @Test
     public void testCodecConstructor() {
         Codec c1 = new Codec("012v", "Uncompressed 4:2:2 10-bit", "D.VI.S");
-        assertThat(c1.name, is("012v"));
-        assertThat(c1.longName, is("Uncompressed 4:2:2 10-bit"));
-        assertThat(c1.canDecode, is(true));
-        assertThat(c1.canEncode, is(false));
-        assertThat(c1.type, is(CodecType.VIDEO));
-        assertThat(c1.intraFrameOnly, is(true));
-        assertThat(c1.lossyCompression, is(false));
+        assertThat(c1.getName(), is("012v"));
+        assertThat(c1.getLongName(), is("Uncompressed 4:2:2 10-bit"));
+        assertThat(c1.getCanDecode(), is(true));
+        assertThat(c1.getCanEncode(), is(false));
+        assertThat(c1.getType(), is(CodecType.VIDEO));
+        assertThat(c1.isIntraFrameOnly(), is(true));
+        assertThat(c1.supportsLossyCompression(), is(false));
         assertThat(c1.losslessCompression, is(true));
         
         Codec c2 = new Codec("4xm", "4X Movie", "D.V.L.");
-        assertThat(c2.name, is("4xm"));
-        assertThat(c2.longName, is("4X Movie"));
-        assertThat(c2.canDecode, is(true));
-        assertThat(c2.canEncode, is(false));
-        assertThat(c2.type, is(CodecType.VIDEO));
-        assertThat(c2.intraFrameOnly, is(false));
-        assertThat(c2.lossyCompression, is(true));
-        assertThat(c2.losslessCompression, is(false));
+        assertThat(c2.getName(), is("4xm"));
+        assertThat(c2.getLongName(), is("4X Movie"));
+        assertThat(c2.getCanDecode(), is(true));
+        assertThat(c2.getCanEncode(), is(false));
+        assertThat(c2.getType(), is(CodecType.VIDEO));
+        assertThat(c2.isIntraFrameOnly(), is(false));
+        assertThat(c2.supportsLossyCompression(), is(true));
+        assertThat(c2.supportsLosslessCompression(), is(false));
 
         Codec c3 = new Codec("alias_pix", "Alias/Wavefront PIX image", "DEVI.S");
-        assertThat(c3.name, is("alias_pix"));
-        assertThat(c3.longName, is("Alias/Wavefront PIX image"));
-        assertThat(c3.canDecode, is(true));
-        assertThat(c3.canEncode, is(true));
-        assertThat(c3.type, is(CodecType.VIDEO));
-        assertThat(c3.intraFrameOnly, is(true));
-        assertThat(c3.lossyCompression, is(false));
-        assertThat(c3.losslessCompression, is(true));
+        assertThat(c3.getName(), is("alias_pix"));
+        assertThat(c3.getLongName(), is("Alias/Wavefront PIX image"));
+        assertThat(c3.getCanDecode(), is(true));
+        assertThat(c3.getCanEncode(), is(true));
+        assertThat(c3.getType(), is(CodecType.VIDEO));
+        assertThat(c3.isIntraFrameOnly(), is(true));
+        assertThat(c3.supportsLossyCompression(), is(false));
+        assertThat(c3.supportsLosslessCompression(), is(true));
 
         Codec c4 = new Codec("binkaudio_rdft", "Bink Audio (RDFT)", "D.AIL.");
-        assertThat(c4.type, is(CodecType.AUDIO));
+        assertThat(c4.getType(), is(CodecType.AUDIO));
 
         Codec c6 = new Codec("mov_text", "MOV text", "DES...");
-        assertThat(c6.type, is(CodecType.SUBTITLE));
+        assertThat(c6.getType(), is(CodecType.SUBTITLE));
 
         Codec c7 = new Codec("bin_data", "binary data", "..D...");
-        assertThat(c7.type, is(CodecType.DATA));
+        assertThat(c7.getType(), is(CodecType.DATA));
     }
 
     @Test(expected = IllegalArgumentException.class)
