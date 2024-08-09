3168a7849fbbd7f9050e09b7d1246c0444274925
Joel Widmer <joel@widmer.me>
2024-08-09 19:53:27 +0200

    test: Add test for -re flag

--- src/test/java/net/bramp/ffmpeg/builder/AbstractFFmpegInputBuilderTest.java
+++ src/test/java/net/bramp/ffmpeg/builder/AbstractFFmpegInputBuilderTest.java
@@ -1,6 +1,23 @@
 package net.bramp.ffmpeg.builder;
 
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public abstract class AbstractFFmpegInputBuilderTest extends AbstractFFmpegStreamBuilderTest {
     @Override
     protected abstract AbstractFFmpegInputBuilder<?> getBuilder();
+
+    @Test
+    public void testReadAtNativeFrameRate() {
+        List<String> command = getBuilder()
+                .readAtNativeFrameRate()
+                .build(0);
+
+        assertThat(removeCommon(command), is(ImmutableList.of("-re")));
+    }
 }
